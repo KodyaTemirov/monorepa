3a51eda8bde72083748805537a0585184d2cd3fc
Kodya Temirov <45668397+KodyaTemirov@users.noreply.github.com>
2022-06-27 11:07:27 +0500

    fix: test husky 3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 import { Button } from 'library';
 
-const App = () => {
+function App() {
   return (
     <>
       <p className="text-3xl font-bold underline">kods</p>
       <Button label="Hello" />
     </>
   );
-};
+}
 
 export default App;
